55a138bddd016cb2554ba831046c5d5ae4e8bd2a
Kavi204 <149049976+Kavi204@users.noreply.github.com>
2023-10-26 13:26:53 +0530

    Add files via upload
    day2

---
+++ day2/Browser.java
@@ -0,0 +1,28 @@
+package week1.day2;
+
+public class Browser {    //Methods and Objects
+	
+	public String launchBrowser(String browserName) {
+		
+	
+		System.out.println("Browser launched Successfully");
+		return browserName;
+		
+
+	}
+	
+	public void loadUrl() {
+		
+		System.out.println("Application url loaded");
+		
+	}
+	/*public static void main(String[] args) {
+		
+		Browser obj = new Browser();
+		obj.launchBrowser("Chrome");
+		obj.loadUrl();
+		
+	}  */
+
+	
+}
